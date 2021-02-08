adadb4e8a826baddced4988baa20aaef2cd88310
Agustin Ruiz <agustin.ruiz@ey.com>
2021-02-07 22:34:52 -0600

    Initial configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom'
+import Users from "./user/pages/users";
+import NewPlace from "./places/pages/new-place";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <Router>
+            <Switch>
+                <Route path="/" exact component={Users}/>
+                <Route path='/places/new' exact component={NewPlace}/>
+                <Redirect to='/'/>
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
